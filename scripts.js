e1f38b0981cd43525ab43990e370ac3aa03afd18
Tentacion-De-Azucar <167883493+Tentacion-De-Azucar@users.noreply.github.com>
2024-04-23 13:04:46 -0230

    Update scripts.js
    a

--- scripts.js
+++ scripts.js
@@ -1,20 +1,22 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // Generar elementos del menú dinámicamente
-    const menu = document.getElementById('menu');
-    for (let i = 1; i <= 30; i++) {
-      const menuItem = document.createElement('div');
-      menuItem.className = 'menu-item';
-      menuItem.innerHTML = `
-        <img src="img/postre${i}.jpeg" alt="Postre ${i}">
-        <p class="description">Descripción del Postre ${i}.</p>
-      `;
-      menuItem.querySelector('img').addEventListener('click', function() {
-        const description = this.nextElementSibling;
-        description.style.display = description.style.display === 'block' ? 'none' : 'block';
-      });
-      menu.appendChild(menuItem);
-    }
-  });
-  
-  // Agregar efectos de fondo con JavaScript o CSS aquí
-  
+  // Generar elementos del menú dinámicamente
+  const menu = document.getElementById('menu');
+  let lastClickedItem = null;
+  for (let i = 1; i <= 30; i++) {
+    const menuItem = document.createElement('div');
+    menuItem.className = 'menu-item';
+    menuItem.innerHTML = `
+      <img src="img/postre${i}.jpeg" alt="Postre ${i}">
+      <p class="description">Descripción del Postre ${i}.</p>
+    `;
+    menuItem.querySelector('img').addEventListener('click', function() {
+      const description = this.nextElementSibling;
+      if (lastClickedItem && lastClickedItem !== description) {
+        lastClickedItem.style.display = 'none';
+      }
+      description.style.display = description.style.display === 'block' ? 'none' : 'block';
+      lastClickedItem = description.style.display === 'block' ? description : null;
+    });
+    menu.appendChild(menuItem);
+  }
+});
